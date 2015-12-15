7b30935e222920be092f5578f792fec9b9811c95
Aron Hegyi <ahegyi@gmail.com>
2015-12-15 11:08:40 -0800

    Add some relationships to Spree::PostalCode

--- app/models/spree/postal_code.rb
+++ app/models/spree/postal_code.rb
@@ -1,5 +1,19 @@
 module Spree
   class PostalCode < Spree::Base
+    belongs_to :state, class_name: 'Spree::State'
+    belongs_to :country, class_name: 'Spree::Country'
+    has_many :addresses, dependent: :nullify
 
+    validates :value, presence: true
+    # FIXME: validate that value is correct for country
+    #        See Spree::Address#postal_code_validate
+
+    def <=>(other)
+      value <=> other.value
+    end
+
+    def to_s
+      value
+    end
   end
 end
