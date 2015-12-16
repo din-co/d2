9d0e3a44ee94c0bc8cda9f0452370b194cecbdb5
Aron Hegyi <ahegyi@gmail.com>
2015-12-16 14:19:55 -0800

    Add more validations for Spree::PostalCode

--- app/models/spree/postal_code.rb
+++ app/models/spree/postal_code.rb
@@ -4,9 +4,8 @@ module Spree
     belongs_to :country, class_name: 'Spree::Country'
     has_many :addresses, dependent: :nullify
 
-    validates :value, presence: true
-    # FIXME: validate that value is correct for country
-    #        See Spree::Address#postal_code_validate
+    validates :value, :country, presence: true
+    validate :postal_code_validate, :us_restrict_digits_validate
 
     def <=>(other)
       value <=> other.value
@@ -15,5 +14,23 @@ module Spree
     def to_s
       value
     end
+
+    private
+
+      # Some code here borrowed from Spree::Address
+      def postal_code_validate
+        return if country.blank? || country.iso.blank?
+        return if !TwitterCldr::Shared::PostalCodes.territories.include?(country.iso.downcase.to_sym)
+
+        postal_code = TwitterCldr::Shared::PostalCodes.for_territory(country.iso)
+        errors.add(:value, :invalid) if !postal_code.valid?(value)
+      end
+
+      # Ensure only five digit postal codes exist for Spree::PostalCodes in US
+      def us_restrict_digits_validate
+        if (country.iso && country.iso.downcase.to_sym == :us)
+          errors.add(:value, :invalid) if value.length > 5
+        end
+      end
   end
 end
