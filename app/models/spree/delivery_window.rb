c4c459901ed3e663f15321e761ca81ba318930e4
Rob LaFave <rob.lafave@gmail.com>
2015-12-30 16:58:55 -0800

    WIP - Add zone admin views

--- app/models/spree/delivery_window.rb
+++ app/models/spree/delivery_window.rb
@@ -1,4 +1,11 @@
 module Spree
   class DeliveryWindow < Spree::Base
+    HOUR_OFFSET = 12
+
+    def to_s
+      range_start = start_hour - HOUR_OFFSET
+      range_end = range_start + duration
+      "#{range_start} to #{range_end}" 
+    end
   end
 end
