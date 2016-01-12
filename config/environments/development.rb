a9f410d5cbb0d4d9b753a25b11115d640e4af3cf
Cameron Walters <cameron.walters@gmail.com>
2016-01-11 23:53:31 -0800

    Enable time travel in development by setting TIME_TRAVEL (handy for delivery windows)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,4 +42,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  Timecop.travel(Time.current.beginning_of_day) if ENV['TIME_TRAVEL'].present?
 end
