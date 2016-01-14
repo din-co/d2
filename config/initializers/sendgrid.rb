ecc5aa698d3fe45ef1be117682218cb3fdbf9045
Cameron Walters <cameron.walters@gmail.com>
2016-01-13 18:36:11 -0800

    Fix sloppy copypasta

--- config/initializers/sendgrid.rb
+++ config/initializers/sendgrid.rb
@@ -12,7 +12,7 @@ if Rails.env.production?
   # Production doesn't send real email unless SEND_REAL_EMAILS is set
   block_real_emails = ENV['SEND_REAL_EMAILS'].blank?
   Rails.application.config.x.redirect_emails_internally = block_real_emails
-  config.action_mailer.show_previews = block_real_emails
+  Rails.application.config.action_mailer.show_previews = block_real_emails
 
   ActionMailer::Base.register_interceptor(RedirectOutgoingEmail)
   ActionMailer::Base.register_interceptor(BccOutgoingEmail)
