694b8c9c0bc642dec33fdf4fe6ee295cd2db87bd
Cameron Walters <cameron.walters@gmail.com>
2016-01-18 11:15:29 -0800

    Configure email host, duh.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,7 @@ Rails.application.configure do
 
   # Do not raise email delivery errors, since emails are processed by a background queue.
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: ENV['EMAIL_HOST'] || 'din.co' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
