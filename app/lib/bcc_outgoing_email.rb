99c18c57a66d067a65a3f4a973071e7c51e20c77
Cameron Walters <cameron.walters@gmail.com>
2016-01-18 08:54:58 -0800

    Safer bcc

--- app/lib/bcc_outgoing_email.rb
+++ app/lib/bcc_outgoing_email.rb
@@ -1,6 +1,7 @@
 class BccOutgoingEmail
   def self.delivering_email(mail)
     return if Rails.configuration.x.redirect_emails_internally
+    mail.bcc ||= []
     mail.bcc += ["app-cc@din.co"]
   end
 end
