2db9433903a62fbc3465c180fa24a763756f73a7
Cameron Walters <cameron.walters@gmail.com>
2016-02-02 21:44:32 -0800

    Unset a flash error if the extended controller has set a notice
    [Fixes #112851703]

--- app/controllers/concerns/spree/user_passwords_controller_concerns.rb
+++ app/controllers/concerns/spree/user_passwords_controller_concerns.rb
@@ -5,12 +5,17 @@ module Spree
     included do
       prepend(InstanceMethods)
 
-      before_filter :default_flash_message, only: [:create]
+      before_action :default_flash_message, only: [:create]
+      after_action :unset_error_when_notice, only: [:create]
     end
 
     module InstanceMethods
       def default_flash_message
         set_flash_message(:error, :email_must_be_valid) if is_navigational_format?
+      end
+
+      def unset_error_when_notice
+        flash.discard(:error) if flash.notice.present?
       end
     end
   end
