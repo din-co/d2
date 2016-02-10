00d67f5ffe8531f72f2d5ef52af9206745c77fab
Beau Smith <beau@beausmith.com>
2016-02-10 13:27:26 -0800

    Add Today’s Menu email.

---
+++ app/controllers/admin/email_controller.rb
@@ -0,0 +1,13 @@
+class Admin::EmailController < Spree::Admin::BaseController
+  include Spree::Core::ControllerHelpers::Order
+  layout "admin/email"
+
+  # prevent auto insert of New Relic js
+  newrelic_ignore_enduser
+
+  def menu
+    @searcher = build_searcher(params.merge(include_images: true))
+    @products = @searcher.retrieve_products
+    @taxonomies = Spree::Taxonomy.includes(root: :children)
+  end
+end
