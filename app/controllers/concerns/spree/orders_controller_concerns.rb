366953988bfa6e2aba353a19a088a2554b987362
Beau Smith <beau@beausmith.com>
2016-02-19 11:20:06 -0800

    Add "More from the menu…" to the cart.

--- app/controllers/concerns/spree/orders_controller_concerns.rb
+++ app/controllers/concerns/spree/orders_controller_concerns.rb
@@ -10,6 +10,12 @@ module Spree
     end
 
     module InstanceMethods
+
+      def edit
+        super
+        @products_not_in_order = Spree::Taxon.pages.find_by(name: "Home").products.where.not(id: @order.product_ids)
+      end
+
       private
 
       def redirect_empty_order_to_menu
