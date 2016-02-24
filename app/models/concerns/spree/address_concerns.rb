e1f8331a9c478e4726ba88d56ff61b9c7c33b85c
Cameron Walters <cameron.walters@gmail.com>
2016-02-24 13:18:03 -0800

    Indentation

--- app/models/concerns/spree/address_concerns.rb
+++ app/models/concerns/spree/address_concerns.rb
@@ -16,27 +16,28 @@ module Spree
     end
 
     private
-      def postal_code_validate
-        super
-        # ensure associated postal_code belongs to country
-        if postal_code.present?
-          if postal_code.country != country
-            if zipcode.present? # reset association
-              self.postal_code = nil
-            else
-              errors.add(:postal_code, :invalid)
-            end
+
+    def postal_code_validate
+      super
+      # ensure associated postal_code belongs to country
+      if postal_code.present?
+        if postal_code.country != country
+          if zipcode.present? # reset association
+            self.postal_code = nil
+          else
+            errors.add(:postal_code, :invalid)
           end
         end
-
-        # ensure at least one of postal_code or zipcode is populated
-        errors.add :postal_code, :blank if postal_code.blank? && zipcode.blank?
       end
 
-      def associate_postal_code
-        return true if zipcode.blank? || country.blank?
-        code = country.try!(:iso) == "US" ? zipcode.to_s.first(5) : zipcode
-        self.postal_code = Spree::PostalCode.find_by(value: code, country: country)
-      end
+      # ensure at least one of postal_code or zipcode is populated
+      errors.add :postal_code, :blank if postal_code.blank? && zipcode.blank?
+    end
+
+    def associate_postal_code
+      return true if zipcode.blank? || country.blank?
+      code = country.try!(:iso) == "US" ? zipcode.to_s.first(5) : zipcode
+      self.postal_code = Spree::PostalCode.find_by(value: code, country: country)
+    end
   end
 end
