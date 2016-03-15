c947f290e4e6b43d3abef344ab4337cffc249a9e
Beau Smith <beau@beausmith.com>
2016-03-15 14:26:25 -0700

    remove unused validations.

--- app/controllers/spree/meal_preferences_controller.rb
+++ app/controllers/spree/meal_preferences_controller.rb
@@ -4,8 +4,6 @@ module Spree
     before_action :authorize_user
     before_action :set_meal_preference
 
-    # before_action :vegetarian_and_no_soy, only: [:update, :create]
-
     def create
       if @meal_preference.update_attributes(meal_preference_params)
         redirect_to spree.account_path
@@ -24,13 +22,6 @@ module Spree
       def meal_preference_params
         params.require(:meal_preference).permit(Spree::MealPreference.diets + Spree::MealPreference.allergens)
       end
-
-      # def vegetarian_and_no_soy
-      #   if @meal_preference.allergen_soybeans && (@meal_preference.diet_tofu || @meal_preference.diet_tempeh)
-      #     flash[:error] = "If you don’t eat soybeans, then you probably shouldn’t select tofu or tempeh."
-      #     redirect_to meal_preferences_path
-      #   end
-      # end
 
       def authorize_user
         unless spree_current_user.present?
