7398569db7597bdec4b99290e135a8b7277b3f63
Cameron Walters <cameron.walters@gmail.com>
2016-03-24 14:43:30 -0700

    Ensure correct validations on meal_count

--- app/models/spree/meal_subscription.rb
+++ app/models/spree/meal_subscription.rb
@@ -4,14 +4,17 @@ module Spree
     belongs_to :delivery_window
 
     validates :user_id, :status, :delivery_day, :delivery_window_id, :meal_count, presence: true
-    validates :meal_count, numericality: { greater_than: 0, less_than: 7, integer: true }
+    validates :meal_count, numericality: { greater_than_or_equal_to: valid_meal_counts.first, less_than_or_equal_to: valid_meal_counts.last, only_integer: true }
 
     enum status: [:enabled, :disabled]
     enum delivery_day: [:monday, :tuesday, :wednesday]
 
-    def valid_meal_counts
-      (1..6).to_a
+    def self.valid_meal_counts
+      @valid_meal_counts ||= (1..6).to_a
     end
 
+    def valid_meal_counts
+      self.class.valid_meal_counts
+    end
   end
 end
