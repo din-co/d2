6fad8868240a394df412bd5e429ba0e37e73339a
Beau Smith <beau@beausmith.com>
2016-03-29 18:48:02 -0700

    User must select at least one notification type.

--- app/models/spree/meal_subscription.rb
+++ app/models/spree/meal_subscription.rb
@@ -9,12 +9,19 @@ module Spree
 
     validates :user_id, :status, :delivery_day, :delivery_window_id, :meal_count, presence: true
     validates :meal_count, numericality: { greater_than_or_equal_to: valid_meal_counts.first, less_than_or_equal_to: valid_meal_counts.last, only_integer: true }
+    validate :valid_notifications
 
     enum status: [:active, :paused]
     enum delivery_day: [:monday, :tuesday, :wednesday]
 
     def valid_meal_counts
       self.class.valid_meal_counts
+    end
+
+    def valid_notifications
+      unless notification_sms || notification_email
+        errors.add(:base, "You must select at least one notification method.")
+      end
     end
   end
 end
