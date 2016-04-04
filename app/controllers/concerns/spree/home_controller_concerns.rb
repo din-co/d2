414c38edf29d7ce90e6fef424eabafaf3bfd3eac
Beau Smith <beau@beausmith.com>
2016-04-04 15:57:51 -0700

    TEST - log cache keys

--- app/controllers/concerns/spree/home_controller_concerns.rb
+++ app/controllers/concerns/spree/home_controller_concerns.rb
@@ -26,7 +26,9 @@ module Spree
       def cache_key_for_products
         count = @products.count
         max_updated_at = (@products.maximum(:updated_at) || Date.today).to_s(:number)
-        "#{I18n.locale}/#{current_currency}/spree/products/all-#{params[:page]}-#{max_updated_at}-#{count}-#{KITCHEN.status}"
+        key = "#{I18n.locale}/#{current_currency}/spree/products/all-#{params[:page]}-#{max_updated_at}-#{count}-#{KITCHEN.status}"
+        puts "HomeControllerConcerns.cache_key_for_products: #{key}"
+        key
       end
 
       def cache_key_for_subscription_products
