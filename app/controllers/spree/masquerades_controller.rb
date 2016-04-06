dc3cd87fe05f06bfa5df9389b5445866510daa34
Cameron Walters <cameron.walters@gmail.com>
2016-04-06 11:12:14 -0700

    Restrict masquerading to only admin users

--- app/controllers/spree/masquerades_controller.rb
+++ app/controllers/spree/masquerades_controller.rb
@@ -1,1 +1,15 @@
-Spree::MasqueradesController = Devise::MasqueradesController
+module Spree
+  class MasqueradesController < Devise::MasqueradesController
+
+    # Prevents any non-admin from masquerading
+    def show
+      unless spree_current_user.try(:admin?)
+        reset_session
+        redirect_to root_path
+        return
+      end
+
+      super
+    end
+  end
+end
