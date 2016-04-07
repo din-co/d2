06c4e8e891a93bc16661ac8c40a19e80f1eac7fd
Cameron Walters <cameron.walters@gmail.com>
2016-04-07 12:26:50 -0700

    Don't select meals in last 3 orders

--- app/lib/meal_selector.rb
+++ app/lib/meal_selector.rb
@@ -10,13 +10,13 @@ class MealSelector
     @available_meals = available_meals
   end
 
-  def selections
-    @selections ||= generate_selections
+  def selections(no_repeats_last_n_orders=3)
+    @selections ||= generate_selections(no_repeats_last_n_orders)
   end
 
   private
 
-    def generate_selections
+    def generate_selections(last_n)
       selections = {}
 
       @meal_preferences.each do |mp|
@@ -32,7 +32,7 @@ class MealSelector
             next unless stock > 0                 # skip meal when out of stock
             next unless mp.allowed? meal          # skip meal when preferences don't match
             next if sub_selections.include? meal  # skip meal if already selected for this subscription
-            # next if user.recently_ordered?(meal)  # (coming soon) skip meal if ordered recently
+            next if user.recently_ordered?(meal, last_n)  # skip meal if ordered recently
             sub_selections << meal                # select meal and reduce stock
             @available_meals[meal] -= 1
             break
