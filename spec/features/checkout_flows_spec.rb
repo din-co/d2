f357a6d585e60ecc54317436fe45e13e30488aa0
Cameron Walters <cameron.walters@gmail.com>
2016-04-21 17:12:53 -0700

    Better test coverage for golden checkout flow

--- spec/features/checkout_flows_spec.rb
+++ spec/features/checkout_flows_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.feature "Checkout flow:" do
-  let(:address) { FactoryGirl.build_stubbed(:address) }
   let(:product) { ProductFactory.create!("Gulf White Shrimp + Caesar + Chicories + Avocado", 30.00, allergens: %w[shellfish], desc: <<-TXT.strip_heredoc) }
     These shrimp are caught from healthy stocks using trawls that allow sea turtles to escape. By buying these shrimp, we are supporting fisheries badly damaged in Hurricane Katrina. Cooling avocado contrasts spicy harissa, a North African condiment, on a salad of bitter greens, all assertive flavors held in perfect balance.
   TXT
@@ -12,9 +11,19 @@ RSpec.feature "Checkout flow:" do
     fill_in "Confirm Password", with: "password"
   end
 
-  def fill_in_credit_card_payment_fields(new_card=true)
+  def fill_in_shipping_address_fields(address)
+    fill_in "First Name", with: address.firstname
+    fill_in "Last Name", with: address.lastname
+    fill_in "Street Address", with: address.address1
+    fill_in "City", with: address.city
+    select address.state.name
+    fill_in "Zip", with: address.zipcode
+    fill_in "Phone", with: address.phone
+    fill_in "Delivery Instructions", with: "Ring the doorbell and then huck the package onto the roof"
+  end
+
+  def fill_in_credit_card_payment_fields(address)
     expect(page).to have_text("Stripe")
-    click_on(new_card ? "Add Card" : "Change Card")
     using_wait_time(6) do # give Stripe more time…
       within_frame(find('.stripe_checkout_app')) do
         fill_in "billing-name", with: "#{address.firstname} #{address.lastname}"
@@ -48,7 +57,7 @@ RSpec.feature "Checkout flow:" do
     expect(page).to have_text(order.user.default_credit_card.last_digits)
   end
 
-  describe "when the kitchen is open" do
+  describe "when the kitchen is open", js: true do
     before do
       Timecop.travel(KITCHEN.opening_time)
       expect(product).to be_available
@@ -75,40 +84,87 @@ RSpec.feature "Checkout flow:" do
       Timecop.return
     end
 
-    scenario "a customer orders a product for delivery to their shipping address", js: true do
-      # Address entry
-      expect(page).to have_current_path(spree.checkout_state_path(:address))
-      address.zipcode = "94110"
-      fill_in "First Name", with: address.firstname
-      fill_in "Last Name", with: address.lastname
-      fill_in "Street Address", with: address.address1
-      fill_in "City", with: address.city
-      select address.state.name
-      fill_in "Zip", with: address.zipcode
-      fill_in "Phone", with: address.phone
-      fill_in "Delivery Instructions", with: "Ring the doorbell and then huck the package onto the roof"
-      click_on "Save and Continue"
+    describe "shipping to inside our delivery area" do
+      let(:address) { FactoryGirl.build_stubbed(:address) }
+      let(:outside_address) { FactoryGirl.build_stubbed(:address, city: "Beverly Hills", zipcode: "90210") }
 
-      # Delivery options
-      expect(page).to have_current_path(spree.checkout_state_path(:delivery))
-      delivery_window = Spree::DeliveryWindow.available.first
-      choose("#{delivery_window.to_s} #{delivery_window.display_cost}")
-      click_on "Save and Continue"
+      scenario "placing an order billed to a new card, using the shipping address, then placing a second order using the same card" do
+        # Address entry
+        expect(page).to have_current_path(spree.checkout_state_path(:address))
+        fill_in_shipping_address_fields(address)
+        click_on "Save and Continue"
 
-      # Payment page -> Order confirmation page
-      fill_in_credit_card_payment_fields
+        # Delivery options
+        expect(page).to have_current_path(spree.checkout_state_path(:delivery))
+        delivery_window = Spree::DeliveryWindow.available.first
+        choose("#{delivery_window.to_s} #{delivery_window.display_cost}")
+        click_on "Save and Continue"
 
-      # Confirm page (make time for Stripe call)
-      using_wait_time(10) { expect(page).to have_current_path(spree.checkout_state_path(:confirm)) }
-      order = Spree::Order.last
-      has_order_details(order)
-      find_button("Place Order", match: :first).click
+        # Payment page
+        fill_in_credit_card_payment_fields(address)
 
-      # Order summary page
-      expect(page).to have_current_path(spree.order_path(order.number))
-      expect(page).to have_text(order.number)
-      has_order_details(order)
+        # Confirm page (make time for Stripe call)
+        using_wait_time(6) { expect(page).to have_current_path(spree.checkout_state_path(:confirm)) }
+        order = Spree::Order.last
+        has_order_details(order)
+        find_button("Place Order", match: :first).click
+
+        # Order summary page
+        expect(page).to have_current_path(spree.order_path(order.number))
+        expect(page).to have_text(order.number)
+        has_order_details(order)
+
+        # Order again (quicker!)
+        visit spree.root_path
+        click_on product.name
+        click_on "Add to Cart (Serves 2) #{product.display_price}"
+        expect(page).to have_current_path(spree.cart_path)
+        click_on "Checkout"
+        expect(page).to have_current_path(spree.checkout_state_path(:address))
+        # No need to fill in shipping details
+        click_on "Save and Continue"
+        expect(page).to have_current_path(spree.checkout_state_path(:delivery))
+        delivery_window = Spree::DeliveryWindow.available.first
+        choose("#{delivery_window.to_s} #{delivery_window.display_cost}")
+        click_on "Save and Continue"
+        # Payment info already filled in
+        click_on "Continue"
+        expect(page).to have_current_path(spree.checkout_state_path(:confirm))
+        order = Spree::Order.last
+        has_order_details(order)
+        find_button("Place Order", match: :first).click
+        expect(page).to have_current_path(spree.order_path(order.number))
+        expect(page).to have_text(order.number)
+        has_order_details(order)
+      end
+
+      scenario "placing an order billed to an address outside our delivery area" do
+        # Address entry
+        expect(page).to have_current_path(spree.checkout_state_path(:address))
+        fill_in_shipping_address_fields(address)
+        click_on "Save and Continue"
+
+        # Delivery options
+        expect(page).to have_current_path(spree.checkout_state_path(:delivery))
+        delivery_window = Spree::DeliveryWindow.available.first
+        choose("#{delivery_window.to_s} #{delivery_window.display_cost}")
+        click_on "Save and Continue"
+
+        # Payment page
+        fill_in_credit_card_payment_fields(outside_address)
+
+        # Confirm page (make time for Stripe call)
+        using_wait_time(6) { expect(page).to have_current_path(spree.checkout_state_path(:confirm)) }
+        order = Spree::Order.last
+        has_order_details(order)
+        find_button("Place Order", match: :first).click
+
+        # Order summary page
+        expect(page).to have_current_path(spree.order_path(order.number))
+        expect(page).to have_text(order.number)
+        has_order_details(order)
+      end
+
     end
-
   end
 end
