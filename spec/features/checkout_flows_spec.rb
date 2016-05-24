0ba425c269ab73f38cd0164408efad9bce490bd8
Cameron Walters <cameron.walters@gmail.com>
2016-05-24 11:25:19 -0500

    Unveil the D

--- spec/features/checkout_flows_spec.rb
+++ spec/features/checkout_flows_spec.rb
@@ -57,7 +57,7 @@ RSpec.feature "Checkout flow:" do
     expect(page).to have_text(Spree.t(order.user.default_credit_card.cc_type))
     expect(page).to have_text(order.user.default_credit_card.last_digits)
 
-    expect(order.shipment_date).to be >= order.completed_at if order.complete?
+    expect(order.shipment_date).to be >= order.completed_at if order.completed?
   end
 
   describe "when the kitchen is open", js: true do
