268b8f5bc1623004d05767a433bf2c7d52cf5c3d
Rob LaFave <rob.lafave@gmail.com>
2016-05-23 23:00:02 -0700

    WIP: remove free shipping promo CTA in checkout. modified shipment_date validation.

--- app/models/concerns/spree/address_concerns.rb
+++ app/models/concerns/spree/address_concerns.rb
@@ -1,3 +1,4 @@
+
 module Spree
   module AddressConcerns
     extend ActiveSupport::Concern
--- app/models/concerns/spree/order_concerns.rb
+++ app/models/concerns/spree/order_concerns.rb
@@ -14,7 +14,7 @@ module Spree
 
       validate :validate_ship_address, if: 'passed_checkout_step?("address")'
 
-      #validates :shipment_date, presence: true, if: 'passed_checkout_step?("delivery")'
+      validate :shipment_date, if: 'passed_checkout_step?("delivery")'
 
       state_machine.before_transition from: :address do |order|
         order.send(:validate_ship_address)
