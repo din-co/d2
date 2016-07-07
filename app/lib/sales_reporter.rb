0c523b9523a1228e69b0e8969f8e5705c48bbfe6
Cameron Walters <cameron.walters@gmail.com>
2016-07-07 15:42:06 -0700

    Log a bit, actually assign restaurants

--- app/lib/sales_reporter.rb
+++ app/lib/sales_reporter.rb
@@ -4,9 +4,10 @@ class SalesReporter
 
     # Which restaurants are in the home taxon?
     restaurants = []
-    Spree::Taxon.homepage.products {|pr| restaurants |= pr.taxons.restaurants}
+    Spree::Taxon.homepage.products.each {|pr| restaurants |= pr.taxons.restaurants}
     # For each of these, send a sales report
     restaurants.each do |restaurant|
+      Rails.logger.info("Sending daily report to #{restaurant.name}")
       Spree::RestaurantMailer.daily_sales_report_email(restaurant, t).deliver_later
     end
   end
