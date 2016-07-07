11a76414a93eab946b175b80f4c70ff2a66bc55c
Cameron Walters <cameron.walters@gmail.com>
2016-07-07 16:39:51 -0700

    Add a schedule method to call run after a short delay

--- app/lib/sales_reporter.rb
+++ app/lib/sales_reporter.rb
@@ -1,7 +1,5 @@
 class SalesReporter
-  def self.run(t = Time.now)
-    # (return unless it's right after midnight)
-
+  def self.run(t = Time.current)
     # Which restaurants are in the home taxon?
     restaurants = []
     Spree::Taxon.homepage.products.each {|pr| restaurants |= pr.taxons.restaurants}
@@ -10,5 +8,11 @@ class SalesReporter
       Rails.logger.info("Sending daily report to #{restaurant.name}")
       Spree::RestaurantMailer.daily_sales_report_email(restaurant, t).deliver_now
     end
+  end
+
+  def self.schedule
+    # TODO: return unless it's right after midnight
+    sleep 90
+    run(Time.current)
   end
 end
